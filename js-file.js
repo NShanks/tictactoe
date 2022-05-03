8bbe0a9c99a9fdb7c6cfadfd6d6f7b2f753bc32d
Nick <nicolasshanks10@gmail.com>
2022-05-02 20:48:11 -0400

    all

--- js-file.js
+++ js-file.js
@@ -10,14 +10,41 @@ function playGame() {
     playerOne.sayHello();
     playerTwo.sayHello();
 
-    let board = document.getElementById('display');
 
     const gameBoard = (() => {//this is now an IIFE
         const boardDiv = document.createElement('boardDiv')
-        gameBoardArray = [0,0,0,0,0,0,0,0,0];
-        //boardDiv = gameBoardArray;
+        let gameBoardArray = [];
         console.log(gameBoardArray);
-        boardDiv.innerHTML = 'board text thing blah blah blah';
-        board.appendChild(boardDiv);
+
+        //create the tiles
+        let show = document.getElementById('display');
+        let gameBoard = document.createElement('tile');
+        for (let i = 0; i<9; i++){
+            gameBoard = document.createElement(`tile`);
+            gameBoard.id = i;
+            gameBoard.innerHTML = ' '
+            show.appendChild(gameBoard);
+            gameBoardArray.push('empty')
+        }
+        
+        // //do something when a tile is clicked
+        // function clickTile() {
+        //     gameBoardArray.splice(event.target, 1, 'X')
+        //     console.log(gameBoardArray)
+        //     console.log(event.target);
+        // }
+
+        //give all tiles a click listener
+        document.querySelectorAll('tile').forEach(tile => {
+            tile.addEventListener('click', (e) => {
+                //clickTile();
+                let clickedTile = e.target.id;
+                console.log(clickedTile)
+                gameBoardArray.splice(clickedTile, 1, 'X')
+                console.log(gameBoardArray)
+                //console.log(event.target);
+            });
+        });
     })();
 }
+//have it place an X when the array changes it to X
